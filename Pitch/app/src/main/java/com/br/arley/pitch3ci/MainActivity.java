db25aaf7effeec667c830c161c74d2200903f603
Arley Novais <nascimentoarle@gmail.com>
2020-03-05 02:22:03 -0400

    Commit 2 added new home screen

--- Pitch/app/src/main/java/com/br/arley/pitch3ci/MainActivity.java
+++ Pitch/app/src/main/java/com/br/arley/pitch3ci/MainActivity.java
@@ -1,18 +1,27 @@
 package com.br.arley.pitch3ci;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btLogar = (Button) findViewById(R.id.bt_entrar);
+        btLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
 
     }
 }
