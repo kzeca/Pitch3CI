110a1400ac0cd50fc063f185262a163c161a71c8
kzeca <jcpeixotoleao@hotmail.com>
2020-03-07 00:58:56 -0400

    TextInputLayout on Login Menu

--- Pitch/app/src/main/java/com/br/arley/pitch3ci/HomeActivity.java
+++ Pitch/app/src/main/java/com/br/arley/pitch3ci/HomeActivity.java
@@ -1,11 +1,8 @@
 package com.br.arley.pitch3ci;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +11,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
     }
 }
